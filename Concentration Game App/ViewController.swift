549c47f042e58d2427310b159988e69e70098c4f
Deshpande <adeshp16@cci0bhgq18aws.dyn.uncc.edu>
2019-06-07 15:39:09 -0400

    Flip Card

--- Concentration Game App/ViewController.swift
+++ Concentration Game App/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     var model = CardModel()
     var cardArray = [Card]()
     
+    var firstFlippedCardIndex:IndexPath?
+    var secondFlippedCard:IndexPath?
+    
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -27,7 +31,58 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        <#code#>
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
+        let card = cardArray[indexPath.row]
+        
+        
+        if card.isFlipped == false{
+            
+            //Flip the card
+            cell.flip()
+            card.isFlipped = true
+            
+            //Determine if it's the first flipped card or second
+            if firstFlippedCardIndex == nil{
+                firstFlippedCardIndex = indexPath
+            }else{
+                
+                //This is the second card being flipped
+                //TODO:  perform matching logic
+            }
+            
+        }
+       
+    }
+    
+    //Mark: Game Logic methods
+    
+    func checkForMatches(_ secondFlippedCardIndex:IndexPath){
+        let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
+        let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as?
+         CardCollectionViewCell
+        
+        let cardOne = cardArray[firstFlippedCardIndex!.row]
+        let cardTwo = cardArray[secondFlippedCardIndex.row]
+        
+        if cardOne.imageName == cardTwo.imageName{
+            
+            //It's a match
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            //Remove the cards from the grid
+            cardOneCell?.remove()
+            cardTwoCell?.remove()
+        }else{
+            
+            //It's not a match
+            
+            //Remove the cards from the grid
+            
+            //Flip both cards back
+        }
+        
+        firstFlippedCardIndex = nil
     }
 }
 
@@ -38,7 +93,10 @@ extension ViewController:UICollectionViewDataSource{
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cardCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        let cardCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        cardCell.setCard(cardArray[indexPath.row])
+        
         return cardCell
     }
     
